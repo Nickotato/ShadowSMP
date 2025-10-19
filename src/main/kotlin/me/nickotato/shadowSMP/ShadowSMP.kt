117a61182e75bd9b63fb9227ac4ae02bc9a74f9c
RealNickPotato <nickflash2654@gmail.com>
2025-10-18 19:58:51 -0500

    Initial commit

---
+++ src/main/kotlin/me/nickotato/shadowSMP/ShadowSMP.kt
@@ -0,0 +1,14 @@
+package me.nickotato.shadowSMP
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ShadowSMP : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
